9c6ff940195e8d1cb578a35020dffa72173daf78
josipbrkan <brkan34@gmail.com>
2025-04-10 01:51:03 +0200

    Initial commit

---
+++ src/main/java/com/jbrkan/tiszadatak/TisZadatakApplication.java
@@ -0,0 +1,13 @@
+package com.jbrkan.tiszadatak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TisZadatakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TisZadatakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jbrkan/tiszadatak/TisZadatakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbrkan.tiszadatak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TisZadatakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
